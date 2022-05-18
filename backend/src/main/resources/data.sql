d5c49b0be15d34d3478009199dd8a8e81779eafd
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-05-18 15:06:49 -0300

    Lessons seed, fix: lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -30,6 +30,24 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-11-20T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-11-20T13:00:00Z', null, true, false);
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do cap 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio: abc', 'https://www.youtube.com/watch?v=p34kaxW0WjU&list=WL&index=79');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do cap 2', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, '', 'https://www.youtube.com/watch?v=p34kaxW0WjU&list=WL&index=79');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do cap 3', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, '', 'https://www.youtube.com/watch?v=p34kaxW0WjU&list=WL&index=79');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do capitulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer trabalho', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2022-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+
+
+
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Tudo certo',TIMESTAMP WITH TIME ZONE '2022-11-20T10:00:00Z', true, null, 1);
 
